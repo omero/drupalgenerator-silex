228e83c9a713c068952de10d9abc40f4b195c2a8
Omar Aguirre <omersguchigu@gmail.com>
2015-03-09 17:59:43 -0600

    separate sections

--- web/js/init.js
+++ web/js/init.js
@@ -1,8 +1,19 @@
 (function($) {
     $(function() {
         $(".button-collapse").sideNav();
-    });
-    $('.collapsible').collapsible({
-        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        $('.collapsible').collapsible({
+            accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+        //Fade when change section
+        $('#sections').children('section').not('#module').hide();
+        $("a").click(function(e){
+            var href = $(this).attr('href');
+            e.preventDefault();
+            $('#sections').children('section').stop().hide();
+            document.location.href = href;
+            $(href).stop().fadeIn("slow");
+            $(this).addClass('active');
+
+        });
     });
 }(jQuery));
